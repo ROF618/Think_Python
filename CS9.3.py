f448e6ec1aeb213869ff21eb30b865e9d7ec0180
Chris <surferbum618@ymail.com>
2018-12-31 00:03:47 -0600

    Finished exercise 9.3 and started a file for 9.4

--- CS9.3.py
+++ CS9.3.py
@@ -1,26 +1,28 @@
 fin = open('word.txt')
 
 
-def avoids(word):
+def avoids(word, forbiden_letters):
     #return true or false
-    print("Please enter the characters you'd like to omit" + " (Please use the following format a,b,c)")
-    forbiden_letters = input()
-    forbiden_letters = forbiden_letters.replace(',', '')
     for letter in word:
         for char in forbiden_letters:
             if char == letter:
                 #the return statement needs to contain logic for deleting the word from the list of words being iterated over
-                return True
+                return False
             else:
                 continue
-    return False
+    return True
 
 #the input function needs to go in the sort_words function so not to keep prompting for user input
 
 def sort_words(words):
+    print("Please enter the characters you'd like to omit" + " (Please use the following format a,b,c)")
+    forbiden_letters = input()
+    forbiden_letters = forbiden_letters.replace(',', '')
     for word in words:
         word.strip()
-        if avoids(word):
+        if avoids(word, forbiden_letters):
             print(word)
+        else:
+            continue
 
 sort_words(fin)
