71516ed4d3d501bde9061927bccf07352e5c3c42
Chris <surferbum618@ymail.com>
2019-01-04 00:21:14 -0600

    exercise 9.4, for loops are working correctly but the if statement needs to be corrected

--- CS9.4.py
+++ CS9.4.py
@@ -2,11 +2,20 @@ fin = open("word.txt")
 
 #function must return true if word contians only letters found in required letters
 def uses_only(word, required_letters):
+    # make the for loop break the word out into single characters and then we will check they match required letters
+    broken_down_word = []
+    required_letters_list = []
+    required_letters = required_letters.replace(',', '')
     for letter in word:
-        for char in required_letters:
-            if char == letter:
-                #the return statement needs to contain logic for deleting the word from the list of words being iterated over
-                return False
-            else:
-                continue
-    return True
+        broken_down_word.append(letter)
+
+    for char in required_letters:
+        required_letters_list.append(char)
+
+    #the if statement is not correct it needs to be tweaked so that if there are repeating letters the function will still return true as long as those letters are present at least once in the word
+    if broken_down_word == required_letters_list:
+        return print('it works')
+    print(broken_down_word)
+    print(required_letters_list)
+
+uses_only('door', 'd,o,r')
