c1ba4dffc3db96b746d496f3b1d29f971a10c683
Chris <surferbum618@ymail.com>
2019-01-07 20:29:16 -0600

    worked a bit on 9.5 but not done, must think through new logic at the end of function

--- CS9.5.py
+++ CS9.5.py
@@ -1,7 +1,10 @@
+
+
 def uses_all(word, required_letters):
     letters_in_word = []
     required_letters_list = []
     required_letters = required_letters.replace(',', '')
+    final_list = []
     for char in word:
         letters_in_word.append(char)
 
@@ -9,12 +12,16 @@ def uses_all(word, required_letters):
 
     for letters in required_letters:
         required_letters_list.append(letters)
-
-
     #the answer to this will be found in https://www.programiz.com/python-programming/methods/built-in/filter
     ## filter first and then use that mutated list to check against the required letters
-    if letters_in_word.isdisjoint(set(required_letters_list)):
-        print("it does not work")
+    for required_letters_list in letters_in_word:
+        if required_letters_list == letters_in_word:
+            final_list.append(required_letters_list)
+    if letters_in_word == final_list:
+        return print("it works")
     else:
-        print("it does work")
-uses_all("door", "d,o,r,e")
+        return print("it doesn't work")
+
+
+
+uses_all("test", "t, e, s")
