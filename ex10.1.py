f405dddcd5452354751ab25a77ef6e469b8efe53
Chris <surferbum618@ymail.com>
2019-01-30 17:35:46 -0600

    worked on exercise 10.1 and finished 10.6

--- ex10.1.py
+++ ex10.1.py
@@ -1,9 +1,12 @@
 def nested_sum(ints):
-    int_nested = []
+    #int_nested = []
     for num in ints:
-        int_nested.append(num)
+        if num != int:
+            for x in list(num):
+                print(x)
+        print(num)
 
 
 
-test = [1, 2, 3, 4, 5, 6]
+test = [1, 2, 3, 4, [22, 24, 25], 5, 6]
 nested_sum(test)
---
+++ ex10.6.py
@@ -0,0 +1,11 @@
+def is_sorted(lineup):
+    sorted_lineup = sorted(lineup)
+
+    if lineup == sorted_lineup:
+        return print("true")
+    else:
+        return print("false")
+
+test = [2, 3, 5, 1, 4, 9, 7]
+test1 = [1, 2, 3, 4]
+is_sorted(test1)
